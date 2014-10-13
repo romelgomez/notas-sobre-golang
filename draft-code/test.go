a737976e87996a14cd6da808f06167b81e58015d
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2014-10-13 16:07:15 -0430

    add test file

---
+++ draft-code/test.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+//	cadeda := "Hola romel"
+//
+//
+//	login := func(name string) (salute string, ok bool) {
+//
+//			fmt.Println(main.cadena);
+//
+//		return //salute, ok
+//	}
+
+
+
+	_, error := 1,false
+
+	if(error){
+
+		c := 0
+
+		for i:=0; i <10; i++ {
+			c+=1
+		}
+
+		fmt.Println(c)
+
+	}else{
+		fmt.Println("Hola");
+	}
+
+
+
+}
