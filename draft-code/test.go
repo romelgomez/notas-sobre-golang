a05dfec891b51d57b96b2749f5bafa36ffde3cd0
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2015-01-29 15:53:45 -0430

    advances of learning - methods

--- draft-code/test.go
+++ draft-code/test.go
@@ -2,57 +2,82 @@ package main
 
 import (
 	"./test"
-
-	"fmt"
 )
 
 
 func main(){
 
-	users := map[string] test.User{
-		"1":{
-			"Romel Javier",
-			"Gomez Herrera",
-			"30",
-		},
-		"2":{
-			"Rudy Alberto",
-			"Gomez Herrera",
-			"25",
-		},
-	}
+		users := test.Users{
+			0:{
+				"Romel Javier",
+				"Gomez Herrera",
+				"30",
+			},
+			1:{
+				"Rudy Alberto",
+				"Gomez Herrera",
+				"25",
+			},
+			2:{
+				"Dilia",
+				"Gomez Herrera",
+				"24",
+			},
+		}
 
-	var user test.User
-	user.Name = "Dilia Alvanelys"
-	user.LastName = "Gomez Herrera"
-	user.Age = "24"
+	  users.Add()
 
-	users["3"] = user
-
-	delete(users, "1")
-
-	if value, exist := users["1"]; exist {
-		fmt.Println(value)
-	} else {
-		fmt.Println("1 no existe")
-	}
-
-
-	for k , v := range users {
-		user := "[Id] " + k + ", [Name] " + v.Name + ", [LastName] " + v.LastName + ", [Age] " + v.Age + ";"
-		fmt.Println(user)
-	}
-
-
-	slice := []test.User{
-		{"Romel", "Gomez", "30"},
-		{"Rudy", "Gomez", "24"},
-		{"Dilia", "Gomez", "25"},
-	}
-
-//	test.RunLoop(slice)
-
-	test.SayHi(slice);
-
+//	var slice2 []int = make([]int, 3, 4)
+//
+//	slice2[0] = 1
+//	slice2[1] = 10
+//	slice2[2] = 12
+//
+//
+//	users := map[string] test.User{
+//		"1":{
+//			"Romel Javier",
+//			"Gomez Herrera",
+//			"30",
+//		},
+//		"2":{
+//			"Rudy Alberto",
+//			"Gomez Herrera",
+//			"25",
+//		},
+//	}
+//
+//	var user test.User
+//	user.Name = "Dilia Alvanelys"
+//	user.LastName = "Gomez Herrera"
+//	user.Age = "24"
+//
+//	user.Add()
+//
+//	users["3"] = user
+//
+//	delete(users, "1")
+//
+//	if value, exist := users["1"]; exist {
+//		fmt.Println(value)
+//	} else {
+//		fmt.Println("1 no existe")
+//	}
+//
+//
+//	for k , v := range users {
+//		user := "[Id] " + k + ", [Name] " + v.Name + ", [LastName] " + v.LastName + ", [Age] " + v.Age + ";"
+//		fmt.Println(user)
+//	}
+//
+//	slice := []test.User{
+//		{"Romel", "Gomez", "30"},
+//		{"Rudy", "Gomez", "24"},
+//		{"Dilia", "Gomez", "25"},
+//	}
+//
+////	test.RunLoop(slice)
+//
+//	test.SayHi(slice);
 
 }
--- draft-code/test/code.go
+++ draft-code/test/code.go
@@ -4,88 +4,101 @@ import (
 	"fmt"
 )
 
+var usersMap = map[int]User{}
+
 type User struct {
 	Name string
 	LastName string
 	Age	string
 }
 
+type Users []User
 
-func RunLoop (data []User){
+func (users Users) Add (){
 
-		for _, v := range data{
-			fmt.Println(v.Name + " " +  v.LastName + " " + v.Age)
-		}
-
-}
-
-
-type Status func(string) ()
-
-func Hi(name, lastName, id string) (salute string, youId string)  {
-	salute = " Hello " + name + " " + lastName
-	youId =	" Your id is: " + id
-	return
-}
-
-func SayHi(user []User){
-
-	for _, v := range user{
-
-		prefix := getPrefix(v.Name)
-
-		salute := " Hello " + prefix + v.Name + " " + v.LastName + ", Your Age is?: " + v.Age
-
-		fmt.Println(salute)
-
+	for k , v := range users {
+		usersMap[k] = v
 	}
 
-}
-
-func getPrefix( name string) ( prefix string) {
-
-	switch name {
-		case "Romel":
-			prefix = "Mr, "
-		case "Dilia":
-			prefix = "Mrs, "
-		default:
-			prefix = "Dude, "
-	}
-
-	return
-}
-
-func TypeSwitchTest (x interface{}){
-
-	switch x.(type) {
-		case int:
-			fmt.Println("is int")
-		case string:
-			fmt.Println("is string")
-		default:
-			fmt.Println("is unknown")
-	}
-
+	fmt.Println(usersMap)
 }
 
 
-
-func Success( text string) {
-	message := "Success: " + text
-	fmt.Println(message)
-}
-
-func Error( text string) {
-	message := "Error: " + text
-	fmt.Println(message)
-}
-
-
-func Message(message string) Status {
-
-	return func(s string){
-		fmt.Println(s + " " + message)
-	}
-
-}
+//func RunLoop (data []User){
+//
+//		for _, v := range data{
+//			fmt.Println(v.Name + " " +  v.LastName + " " + v.Age)
+//		}
+//
+//}
+//
+//
+//type Status func(string) ()
+//
+//func Hi(name, lastName, id string) (salute string, youId string)  {
+//	salute = " Hello " + name + " " + lastName
+//	youId =	" Your id is: " + id
+//	return
+//}
+//
+//func SayHi(user []User){
+//
+//	for _, v := range user{
+//
+//		prefix := getPrefix(v.Name)
+//
+//		salute := " Hello " + prefix + v.Name + " " + v.LastName + ", Your Age is?: " + v.Age
+//
+//		fmt.Println(salute)
+//
+//	}
+//
+//}
+//
+//func getPrefix( name string) ( prefix string) {
+//
+//	switch name {
+//		case "Romel":
+//			prefix = "Mr, "
+//		case "Dilia":
+//			prefix = "Mrs, "
+//		default:
+//			prefix = "Dude, "
+//	}
+//
+//	return
+//}
+//
+//func TypeSwitchTest (x interface{}){
+//
+//	switch x.(type) {
+//		case int:
+//			fmt.Println("is int")
+//		case string:
+//			fmt.Println("is string")
+//		default:
+//			fmt.Println("is unknown")
+//	}
+//
+//}
+//
+//
+//
+//func Success( text string) {
+//	message := "Success: " + text
+//	fmt.Println(message)
+//}
+//
+//func Error( text string) {
+//	message := "Error: " + text
+//	fmt.Println(message)
+//}
+//
+//
+//func Message(message string) Status {
+//
+//	return func(s string){
+//		fmt.Println(s + " " + message)
+//	}
+//
+//}
