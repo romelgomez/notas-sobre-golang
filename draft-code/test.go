b70f4ffc09e86b4376cd39b53b5ff3be5183fea5
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2015-01-29 09:58:15 -0430

    advances of learning - maps

--- draft-code/test.go
+++ draft-code/test.go
@@ -3,10 +3,46 @@ package main
 import (
 	"./test"
 
+	"fmt"
 )
 
 
 func main(){
+
+	users := map[string] test.User{
+		"1":{
+			"Romel Javier",
+			"Gomez Herrera",
+			"30",
+		},
+		"2":{
+			"Rudy Alberto",
+			"Gomez Herrera",
+			"25",
+		},
+	}
+
+	var user test.User
+	user.Name = "Dilia Alvanelys"
+	user.LastName = "Gomez Herrera"
+	user.Age = "24"
+
+	users["3"] = user
+
+	delete(users, "1")
+
+	if value, exist := users["1"]; exist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("1 no existe")
+	}
+
+
+	for k , v := range users {
+		user := "[Id] " + k + ", [Name] " + v.Name + ", [LastName] " + v.LastName + ", [Age] " + v.Age + ";"
+		fmt.Println(user)
+	}
+
 
 	slice := []test.User{
 		{"Romel", "Gomez", "30"},
