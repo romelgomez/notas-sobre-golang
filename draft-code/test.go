bb35850cd734d92e6adcc4f02ac37701f8e972b0
romelgomez <bmxquiksilver7185@gmail.com>
2015-04-26 21:26:52 -0430

    some changes

--- draft-code/test.go
+++ draft-code/test.go
@@ -1,31 +1,243 @@
 package main
 
 import (
-	"./test"
+	"fmt"
 )
 
+//basic types
+//	bool
+//	string
+//	int, int8, int16, int32, int64
+//	uint,uint8, uint16, uint32, uint64, uintptr
+//	byte (uint8)
+//	rune (int32), like a char
+//	float32,float64
+//	complex64,complex128
+//Other types
+//	array
+//	slice
+//	struct
+//	pointer
+//	function
+//	interface
+//	map
+//	channel
 
-func main(){
+func boolExample(){
+	var bol = false
+	fmt.Println(bol)
+}
 
-		users := test.Users{
-			0:{
-				"Romel Javier",
-				"Gomez Herrera",
-				"30",
-			},
-			1:{
-				"Rudy Alberto",
-				"Gomez Herrera",
-				"25",
-			},
-			2:{
-				"Dilia",
-				"Gomez Herrera",
-				"24",
-			},
+func stringExample(){
+
+	// 1 form
+	var message string
+	message = "Hello Word"
+
+	// 2 form
+	var message3 string = "Hello Word 3"
+
+	// 3 form
+	message2 := "Hello Word 2"
+
+	// 1 form
+	var a, b, c int
+
+	// 2 form
+	var a2, b2, c2 int = 1, 2, 3
+
+	// 3 form
+	a3, b3, c3 := 1, false, 3
+
+	fmt.Println(message)
+	fmt.Println(message2)
+	fmt.Println(message3)
+	fmt.Println(a, b, c)
+	fmt.Println(a2, b2, c2)
+	fmt.Println(a3, b3, c3)
+
+}
+
+func intExample(){
+//	var a,b,c int
+//	var a,b,c int = 1,2,3
+}
+
+func uintExample() {
+
+}
+
+func byteExample() {
+
+}
+
+func runeExample() {
+
+}
+
+func floatExample() {
+
+}
+
+func complexExample() {
+
+}
+
+func typeExample() {
+	type mockString string
+
+	var randomVar mockString = "Type Example"
+
+	fmt.Println(randomVar)
+}
+
+
+func arrayExample(){
+	
+}
+
+func sliceExample(){
+	
+}
+
+func structExample(){
+
+	type User struct {
+		name string
+		lastName string
+	}
+
+	// 1 form
+	randomUser := User{
+		"romel",
+		"Gomez",
+	}
+
+	// 2 form
+	var randomUser2 User
+	randomUser2.name 		= "javier"
+	randomUser2.lastName	= "herrera"
+
+	fmt.Println(randomUser)
+	fmt.Println(randomUser2)
+
+	// 3 form
+	type User2 struct {
+		name string
+		lastName string
+		pc struct {
+			box string
+			memory string
+			procesador string
 		}
+	}
 
-	  users.Add()
+	var randomUser3 User2
+
+	randomUser3.name = "romel"
+	randomUser3.lastName = "Gomez"
+	randomUser3.pc.box = "corsair 800D"
+	randomUser3.pc.memory = "intel"
+	randomUser3.pc.procesador = "core 2duo"
+
+
+	fmt.Println(randomUser3)
+
+
+}
+
+func pointerExample(){
+//	un tipo de variable especial que mantiene la dirección de otra variable en memoria
+	hello := "Hello Word"
+	pointer := &hello
+	*pointer = "Hello Romel"
+
+	fmt.Println(hello, pointer, *pointer)
+}
+
+
+type User struct {
+	Name string
+	LastName string
+	Age	string
+}
+
+func returningMultipleValues(user User) (userData string, plusData string){
+	userData = "Name: " + user.Name + ", LastName:" + user.LastName + ", Age:" + user.Age
+	plusData = "This day is sunday"
+	return
+}
+
+func functionExample(){
+
+	var randomUser User
+
+	randomUser.Name 		= "romel"
+	randomUser.LastName 	= "javier"
+	randomUser.Age 			= "30"
+
+	printThis,_ := returningMultipleValues(randomUser)
+
+	fmt.Println(printThis)
+
+
+
+}
+	
+func interfaceExample(){
+	
+}
+
+func mapExample() {
+
+	type User struct {
+		Name string
+		LastName string
+		Age	string
+	}
+
+	type Users []User
+
+	users := Users{
+		0:{
+			"Romel Javier",
+			"Gomez Herrera",
+			"30",
+		},
+		1:{
+			"Rudy Alberto",
+			"Gomez Herrera",
+			"25",
+		},
+		2:{
+			"Dilia",
+			"Gomez Herrera",
+			"24",
+		},
+	}
+
+	fmt.Println(users)
+
+
+	//	var usersMap = map[int]User{}
+
+
+//	func (users Users) Add (){
+//		for k , v := range users {
+//			usersMap[k] = v
+//		}
+//		fmt.Println(usersMap)
+//	}
+//
+//
+//	users.Add()
+
+
+}
+
+func channelExample() {
+
+}
 
 //	var slice2 []int = make([]int, 3, 4)
 //
@@ -36,7 +248,7 @@ func main(){
 //
 //	users := map[string] test.User{
 //		"1":{
-//			"Romel Javier",
+//			"Romel Javier",n
 //			"Gomez Herrera",
 //			"30",
 //		},
@@ -80,4 +292,15 @@ func main(){
 //
 //	test.SayHi(slice);
 
+
+
+func main(){
+
+//	boolExample()
+//	stringExample()
+//	mapExample()
+//	pointerExample()
+//	typeExample()
+//	structExample()
+	functionExample()
 }
